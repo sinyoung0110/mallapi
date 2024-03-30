582ef1fc1869ecd37a3f4bab8e1290808af8da1b
sinyoung0110 <dpqlc7008@naver.com>
2024-03-31 01:08:02 +0900

    test 만들기

---
+++ src/main/java/org/zerock/mallapi/domain/Todo.java
@@ -0,0 +1,50 @@
+package org.zerock.mallapi.domain;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="tbl_todo")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long tno;
+    @Column(length = 500, nullable=false)
+
+    private String title;
+    private String content;
+    private boolean complete;
+    private LocalDate dueDate;
+
+    public void changeTitle(String title) {
+        this.title = title;
+    }
+
+    public void changeContent(String content) {
+        this.content = content;
+    }
+
+    public void changeComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    public void changeDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+}
---
+++ src/test/java/org/zerock/mallapi/repository/TodoRepositoryTests.java
@@ -0,0 +1,82 @@
+package org.zerock.mallapi.repository;
+
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.zerock.mallapi.domain.Todo;
+
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+@Log4j2
+public class TodoRepositoryTests{
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Test
+    public void test1(){
+        Assertions.assertNotNull(todoRepository);
+        log.info(todoRepository.getClass().getName());
+    }
+
+    @Test
+    public void testInsert(){
+
+        for(int i=0;i<100;i++) {
+
+
+            Todo todo = Todo.builder()
+                    .title("Title"+i)
+                    .content("content.."+i)
+                    .dueDate(LocalDate.of(2024, 3, 31))
+                    .build();
+
+            Todo result = todoRepository.save(todo);
+            log.info(result);
+        }
+    }
+
+    @Test
+    public void testRead(){
+        Long tno=1L;
+        Optional<Todo> result=todoRepository.findById(tno);
+        Todo todo=result.orElseThrow();
+        log.info(todo);
+    }
+    @Test
+    public void testUpdate(){
+        Long tno=1L;
+        Optional<Todo> result=todoRepository.findById(tno);
+        Todo todo=result.orElseThrow();
+
+        todo.changeTitle("Update Title");
+        todo.changeContent("updated ocntent");
+        todo.changeComplete((true));
+
+        todoRepository.save(todo);
+
+    }
+
+    @Test
+    public void testPaging(){
+        //페이지 번호는 0부터
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("tno").descending());
+
+        Page<Todo> result=todoRepository.findAll(pageable);
+        log.info(result.getTotalElements());
+
+        log.info(result.getContent());
+    }
+
+
+}
+
